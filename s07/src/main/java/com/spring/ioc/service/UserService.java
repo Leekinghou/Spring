049b00e057644fe6184f1790b8fefce70afc9426
leekinghou <>
2022-02-18 14:10:02 +0800

    基于注解配置IoC容器

---
+++ s07/src/main/java/com/spring/ioc/service/UserService.java
@@ -0,0 +1,13 @@
+package com.spring.ioc.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: lijinhao
+ * @date: 2022/02/18 13:39
+ */
+
+@Service
+public class UserService {
+
+}
