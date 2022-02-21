0df1abdbf3374bc48ed6d02b25ba0442804b7561
leekinghou <>
2022-02-21 12:25:48 +0800

    :ambulance: Spring Test测试用例

---
+++ s09/src/main/java/com/spring/ioc/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.spring.ioc.dao;
+
+/**
+ * @author: lijinhao
+ * @date: 2022/02/21 12:11
+ */
+
+public class UserDao {
+
+    public void insert(){
+        System.out.println("新增用户数据");
+    }
+}
