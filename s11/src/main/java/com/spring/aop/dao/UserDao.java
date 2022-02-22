5e3ee3fd66af47ff088e25ed4f3ede2851b79e63
leekinghou <>
2022-02-22 20:06:31 +0800

    :green_heart: 环绕通知around advice

---
+++ s11/src/main/java/com/spring/aop/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.spring.aop.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * 用户表Dao
+ */
+
+@Repository
+public class UserDao {
+    public void insert(){
+        System.out.println("新增用户数据");
+    }
+}
